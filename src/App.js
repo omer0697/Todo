fc0efe39fba101aad85c2f9c4fc477b1e76c31b3
omer9706 <omer_yurtseven1997@outlook.com>
2022-09-21 19:25:56 +0300

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from "react"
 function App() {
+  const [todo,setTodo] = useState("")
+  const [todos,setTodos] = useState([])
+
+  const submitHandle = (e) => {
+    e.preventDefault()
+    setTodos([...todos,todo])
+    setTodo("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={submitHandle}>
+        <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)}></input>
+        <button disabled={!todo} type="submit">Ekle</button>
+      </form>
+      {todos.map((todo,index)=>(
+        <li key={index}>{todo}</li>
+      ))}
+
+      
     </div>
   );
 }
